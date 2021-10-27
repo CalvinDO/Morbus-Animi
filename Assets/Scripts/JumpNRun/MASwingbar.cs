81b87252319983e490e21ae9c91aa0c84b600e43
Calvin <calvindelloro@mail.de>
2021-10-27 11:13:18 +0200

    feature(CharacterController): Swing improved, swinging and releasing is now possible

--- Assets/Scripts/JumpNRun/MASwingbar.cs
+++ Assets/Scripts/JumpNRun/MASwingbar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MASwingbar : MonoBehaviour {
     public Rigidbody rb;
-
+    private GameObject currentRotator;
 
     void Start() {
 
@@ -19,20 +19,29 @@ public class MASwingbar : MonoBehaviour {
 
         characterController.rb.isKinematic = true;
 
-        GameObject characterRotator = new GameObject();
-        characterRotator.name = "CharacterRotator";
+        this.currentRotator = new GameObject();
+        this.currentRotator.name = "CurrentRotator";
 
-        characterRotator.transform.SetPositionAndRotation(characterController.swingGrabPosition.position, characterController.swingGrabPosition.rotation);
+        this.currentRotator.transform.SetPositionAndRotation(characterController.swingGrabPosition.position, characterController.swingGrabPosition.rotation);
 
-        characterRotator.transform.SetParent(this.transform);
 
-        characterController.transform.SetParent(characterRotator.transform);
-        characterRotator.transform.LookAt(this.transform);
+        characterController.transform.SetParent(this.currentRotator.transform);
+        this.currentRotator.transform.LookAt(this.transform);
 
-        Vector3 handToSwingbar = this.transform.position - characterRotator.transform.position;
-        characterRotator.transform.Translate(handToSwingbar);
+        this.transform.LookAt(characterController.transform);
+        this.currentRotator.transform.SetParent(this.transform);
+
+        Vector3 handToSwingbar = this.transform.position - this.currentRotator.transform.position;
+        this.currentRotator.transform.Translate(handToSwingbar);
 
 
         this.rb.centerOfMass = characterController.transform.position;
+    }
+
+    public void ReleaseCharacter(MACharacterController characterController) {
+        characterController.transform.SetParent(null);
+        characterController.rb.isKinematic = false;
+        GameObject.Destroy(this.currentRotator);
+        this.currentRotator = null;
     }
 }
