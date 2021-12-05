a5422e5c540d392b606a9eabd6c9406c0c04b139
bajergis <64094239+bajergis@users.noreply.github.com>
2021-12-05 19:07:13 +0100

    feature(pushObject) implemented pushing

---
+++ Assets/Scripts/Interaction/MAPressurePlate.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAPressurePlate : MonoBehaviour
+{
+    public int counter;
+    public GameObject test;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<MAPushable>())
+        {
+            counter++;
+            Debug.Log(counter);
+            // call whatever flo wants to do with this mechanic
+            test.transform.position += new Vector3(1,0,0);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<MAPushable>())
+        {
+            counter--;
+            Debug.Log(counter);
+            if (counter == 0)
+            {
+                test.transform.position -= new Vector3(1, 0, 0);
+            }
+        }
+    }
+}
