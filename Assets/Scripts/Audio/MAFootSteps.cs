2d9c15dc1987cdf62e92bdf40be2d13780ff0c00
Calvin <calvindelloro@mail.de>
2022-01-14 01:31:33 +0100

    feature(SoundEvents): added footsteps to walk and run for norah and darknorah (entity)

---
+++ Assets/Scripts/Audio/MAFootSteps.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MAFootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] stoneClips;
+
+    private AudioSource audioSource;
+    private TerrainDetector terrainDetector;
+
+    private void Awake()
+    {
+        this.audioSource = GetComponent<AudioSource>();
+        //this.terrainDetector = new TerrainDetector();
+    }
+
+    private void Step()
+    {
+
+        Debug.Log("step!");
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip()
+    {
+
+        if (this.stoneClips.Length <= 0) {
+            Debug.LogWarning("No Stone Clips assigned!");
+            return null;
+        }
+
+
+        
+        return this.stoneClips[UnityEngine.Random.Range(0, stoneClips.Length)];    
+    }
+}
