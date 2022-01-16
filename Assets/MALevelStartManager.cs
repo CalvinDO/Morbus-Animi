a39252c7e7ac86d48e96d4ce116fd28eed1a6304
Calvin <calvindelloro@mail.de>
2022-01-16 20:53:22 +0100

    feature(StartManager, TitleTheme): removed setting character pos to 0 0 0 and added titlethemetrigger

--- Assets/MALevelStartManager.cs
+++ Assets/MALevelStartManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class MALevelStartManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        MACharacterController characterController = GameObject.Find("SmallNorah").GetComponent<MACharacterController>();
-        characterController.transform.position = Vector3.zero;
+        //MACharacterController characterController = GameObject.Find("SmallNorah").GetComponent<MACharacterController>();
+       // characterController.transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
