7fee44088fba7669cd08a310062413d53eca1ac6
Florian Malitschenko <florian.malitschenko@hs-furtwangen.de>
2022-01-20 21:06:03 +0200

    Added new Menu Scene

---
+++ Assets/Scripts/UI/MAMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MAMenu : MonoBehaviour
+
+
+
+    
+{
+
+    public string firstLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void OpenOptions()
+    { }
+
+
+    public void CloseOptions()
+    { }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
