712cb2896c9b973a05e66e562bfd2b9fdc3ad502
Calvin <calvindelloro@mail.de>
2022-01-22 20:18:44 +0100

    fix(uiappear): removed tag check, it is not needed, because layer is onlyCharacterCollision

--- Assets/Scripts/UI/MAUIAppear.cs
+++ Assets/Scripts/UI/MAUIAppear.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MAUIAppear : MonoBehaviour
-{
+public class MAUIAppear : MonoBehaviour {
     [SerializeField] private Image customImage;
 
-    void OnTriggerEnter(Collider other)
-    {
-        if(other.CompareTag("MA_Character"))
-        {
-            customImage.enabled = true;
-        }
-    }
-    
+    void OnTriggerEnter(Collider other) {
 
-        void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("MA_Character"))
-        {
-            customImage.enabled = false;
-        }
+        customImage.enabled = true;
+
+    }
+
+
+    void OnTriggerExit(Collider other) {
+
+        customImage.enabled = false;
+
     }
 
 }
